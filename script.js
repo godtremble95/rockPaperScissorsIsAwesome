797c6f604a7ba2a1e61be16f0d5a60d5c9fcea44
godtremble95 <116137971+godtremble95@users.noreply.github.com>
2023-01-05 22:32:53 -0500

    Play's 5 rounds of game. Keeps player's score.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const choices = ['rock', 'paper', 'scissors'];
+let playerScore = 0;
 
 // get computer's choice
 function getComputerChoice() {
@@ -6,7 +7,24 @@ function getComputerChoice() {
     return compSelect;
 }
 
-// prompt user to choose rock, paper, or scissors
+function game() {
+    // play 5 round of the game
+    for (i = 0; i < 5; i++) {
+        let computerSelection = getComputerChoice();
+        // prompt user to choose rock, paper, or scissors
+        let playerSelection = prompt(`Choose ROCK, PAPER, or SCISSORS:\nGame ${i + 1} of 5`);
+        if (playerSelection === null) {break;} 
+        playerSelection = playerSelection.toLowerCase();
+        while (!choices.includes(playerSelection)) {
+            playerSelection = prompt('Choice must be ROCK, PAPER, or SCISSORS')
+            if (playerSelection === null) {continue;}
+            playerSelection = playerSelection.toLowerCase();
+        }
+        console.log(playRound(playerSelection, computerSelection));
+    }
+    console.log(`Game Over!\nScore: ${playerScore}`)
+}
+
 // compare results and log answer
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
@@ -17,18 +35,21 @@ function playRound(playerSelection, computerSelection) {
             if (computerSelection === 'paper') {
                 return `You Lose! ${computerSelection} beats ${playerSelection}`
             };
+            playerScore++
             return `You Win! ${playerSelection} beats ${computerSelection}`;
 
         case 'paper':
             if (computerSelection === 'scissors') {
                 return `You Lose! ${computerSelection} beats ${playerSelection}`
             };
+            playerScore++
             return `You Win! ${playerSelection} beats ${computerSelection}`;
 
         case 'scissors':
             if (computerSelection === 'rock') {
                 return `You Lose! ${computerSelection} beats ${playerSelection}`
             };
+            playerScore++
             return `You Win! ${playerSelection} beats ${computerSelection}`;
 
         default:
@@ -36,6 +57,4 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-const playerSelection = 'scissors';
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+game();
