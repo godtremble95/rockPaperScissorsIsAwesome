d7170915e98a7999f9152120c65082ad6b628269
godtremble95 <116137971+godtremble95@users.noreply.github.com>
2023-01-07 20:42:39 -0500

    Added UI -- Game now plays outside console

--- script.js
+++ script.js
@@ -1,16 +1,58 @@
-const choices = ['rock', 'paper', 'scissors'];
+const buttons = document.querySelectorAll('button');
+const results = document.getElementById('results');
+
 let playerScore = 0;
 let computerScore = 0;
-let computerSelection;
-let playerSelection
 
 // get computer's choice
 function getComputerChoice() {
-    const compSelect = choices[Math.floor(Math.random() * 3)]
-    return compSelect;
+  const choices = ['rock', 'paper', 'scissors'];
+  const compSelect = choices[Math.floor(Math.random() * 3)]
+  return compSelect;
 }
 
-function game() {
+// initialize selections and display answer/score
+function playRound() {
+  const playerSelection = this.id;
+  const computerSelection = getComputerChoice();
+  results.textContent = getResult(playerSelection, computerSelection);
+}
+
+// decide winner and award points
+function getResult(playerSelection, computerSelection){
+  let win;
+  if (playerSelection === computerSelection) {
+    return `Draw!\nPlayer: ${playerScore}\tComputer: ${computerScore}`;
+  }
+  switch (playerSelection) {
+    case 'rock':
+      win = (computerSelection === 'scissors')? true : false;
+      break;
+    case 'paper':
+      win = (computerSelection === 'rock')? true : false;
+      break;
+    case 'scissors':
+      win = (computerSelection === 'paper')? true : false;
+      break;
+    default:
+      console.log('Something went very, very wrong :(');
+  }
+  switch (win){
+    case true:
+      playerScore++
+      return `You Win! ${playerSelection.replace(playerSelection[0], playerSelection[0].toUpperCase())} Beats ${computerSelection.replace(computerSelection[0], computerSelection[0].toUpperCase())}\nPlayer: ${playerScore}\tComputer: ${computerScore}`;
+    case false:
+      computerScore++
+      return `You Lose! ${computerSelection.replace(computerSelection[0], computerSelection[0].toUpperCase())} Beats ${playerSelection.replace(playerSelection[0], playerSelection[0].toUpperCase())}\nPlayer: ${playerScore}\tComputer: ${computerScore}`;
+    default:
+      console.log('Something went very, very wrong :(');
+  }
+}
+
+//Create Listener for player's selection
+buttons.forEach(button => button.addEventListener('click', playRound));
+
+/* function game() {
     // play 5 round of the game
     for (i = 0; i < 5; i++) {
         computerSelection = getComputerChoice();
@@ -29,36 +71,4 @@ function game() {
                    (playerScore < computerScore)? "Computer is victorious!":
                    "It's a draw!";
     console.log(`Game Over!\nPlayer: ${playerScore}\tComputer: ${computerScore}\n${winner}`);
-}
-
-// compare results and log answer
-function playRound() {
-    if (playerSelection === computerSelection) {
-        return didWin();
-    }
-    switch (playerSelection) {
-        case 'rock':
-            return (computerSelection === 'scissors')? didWin(true) : didWin(false);
-        case 'paper':
-            return (computerSelection === 'rock')? didWin(true) : didWin(false);
-        case 'scissors':
-            return (computerSelection === 'paper')? didWin(true) : didWin(false);
-        default:
-            return 'Something went very, very wrong :(';
-    }
-}
-
-function didWin(win){
-    switch (win){
-        case true:
-            playerScore++
-            return `You Win! ${playerSelection.replace(playerSelection[0], playerSelection[0].toUpperCase())} Beats ${computerSelection.replace(computerSelection[0], computerSelection[0].toUpperCase())}\nPlayer: ${playerScore}\tComputer: ${computerScore}`;
-        case false:
-            computerScore++
-            return `You Lose! ${computerSelection.replace(computerSelection[0], computerSelection[0].toUpperCase())} Beats ${playerSelection.replace(playerSelection[0], playerSelection[0].toUpperCase())}\nPlayer: ${playerScore}\tComputer: ${computerScore}`;
-        default:
-            return `Draw!\nPlayer: ${playerScore}\tComputer: ${computerScore}`;
-    }
-}
-
-game();
+} */
